0b6d943aa3405f1b17f7e8b8785e2382d565fd72
dariak <dariak@playtika.com>
2020-10-16 11:53:49 +0300

    Empty Project

---
+++ DoTweenProject/Assets/MenuManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private Transform centerPoint;
+    [SerializeField] private Transform pausePanel;
+    [SerializeField] private Transform playButton;
+    
+    private float pausePanelStartY;
+    private bool isPause;
+    
+    void Start()
+    {
+        pausePanelStartY = pausePanel.position.y;
+        pausePanel.localScale = Vector3.zero;
+    }
+
+    public void ShowPauseMenu()
+    {
+        var positionY = isPause ? pausePanelStartY : centerPoint.position.y;
+        pausePanel.DOMoveY(positionY, 1f);
+        
+        var scale = isPause ? Vector3.zero : Vector3.one;
+        pausePanel.DOScale(scale, 1f);
+
+        if (isPause)
+        {
+            var sequence = DOTween.Sequence();
+            sequence.Append(playButton.DOScale(Vector3.one, 0.5f).SetEase(Ease.InBack));
+            sequence.Append(playButton.DOShakeRotation(0.5f, new Vector3(0,0,120f), 10, 30, true));
+        }
+        else
+        {
+            playButton.DOScale(Vector3.zero, 0.5f);
+        }
+        
+        isPause = !isPause;
+    }
+}
---
+++ DoTweenProject/Assets/Route.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Route : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform[] points;
+
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button stopButton;
+
+    private Tweener _tweener;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(Play);
+        stopButton.onClick.AddListener(Stop);
+    }
+
+    private void Play()
+    {
+        var path = new Vector3[points.Length];
+        for (int i=0; i<points.Length; i++)
+        {
+            path[i] = points[i].transform.position;
+        }
+
+        _tweener = target.DOPath(path, 5f);
+    }
+
+    private void Stop()
+    {
+        _tweener?.Kill();
+    }
+}

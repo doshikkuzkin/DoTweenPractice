8824c3d5d2aaf66cd86c704ba3fee8063c4af729
dariak <dariak@playtika.com>
2020-10-16 13:41:49 +0300

    Fix Load2

--- DoTweenProject/Assets/ScaleRotateLoad.cs
+++ DoTweenProject/Assets/ScaleRotateLoad.cs
@@ -14,15 +14,28 @@ public class ScaleRotateLoad : MonoBehaviour
 
     private void Start()
     {
+        transform.DORotate(rot1, 1f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
         Sequence sequence = DOTween.Sequence();
-        sequence.Append(transform.DORotate(rot1, 1f, RotateMode.FastBeyond360)
-            .SetEase(Ease.Linear)).SetLoops(-1, LoopType.Restart)
-            .Join(image1.DOScale(Vector3.zero, 1f))
+        sequence.Append(image1.DOScale(Vector3.one, 0.5f))
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Restart)
+            .Append(image1.DOScale(Vector3.zero, 0.5f))
+            .SetEase(Ease.Linear)
             .SetLoops(-1, LoopType.Restart);
+        
+        
         Sequence seq2 = DOTween.Sequence();
-        seq2.Append(axis.DORotate(rot1, 0.75f, RotateMode.FastBeyond360)
-                .SetEase(Ease.Linear)).SetLoops(-1, LoopType.Restart)
-            .Join(image2.DOScale(Vector3.zero, 0.75f))
-            .SetLoops(-1, LoopType.Restart).AppendInterval(0.25f);
+        seq2.Append(axis.DORotate(rot1, 0.9f, RotateMode.FastBeyond360)).SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Restart)
+            .AppendInterval(0.1f);
+        
+        Sequence seq3 = DOTween.Sequence();
+        seq3.Append(image2.DOScale(Vector3.one, 0.45f))
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Restart)
+            .Append(image2.DOScale(Vector3.zero, 0.45f))
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Restart)
+            .AppendInterval(0.1f);
     }
 }
